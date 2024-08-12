87b93c049a02ab7046b033397b84ba236fb458ad
lucascody <lucassantiagocody@gmail.com>
2024-08-12 16:46:56 +0100

    Create Patient AbstractUserRole

--- core/roles.py
+++ core/roles.py
@@ -1,1 +1,14 @@
 from rolepermissions.roles import AbstractUserRole
+
+
+class Patient(AbstractUserRole):
+    available_permissions = {
+        'can_add_profile': True,
+        'can_view_profile': True,
+        'can_change_profile': True,
+        'can_delete_profile': True,
+        'can_add_consultation': True,
+        'can_view_consultation': True,
+        'can_change_consultation': True,
+        'can_delete_consultation': True,
+    }   
