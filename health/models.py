cdaf473d395734577861c17961ee81219a02916d
lucascody <lucassantiagocody@gmail.com>
2024-08-13 15:58:22 +0100

    Create BasicInformation model in health app

--- health/models.py
+++ health/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from .models_choices import INSTITUTION_TYPE
 
-# Create your models here.
+
+class BasicInformation(models.Model):
+    institution_name = models.CharField(max_length=150)
+    nif = models.CharField(max_length=14)
+    institution_type = models.CharField(max_length=3, choices=INSTITUTION_TYPE)
