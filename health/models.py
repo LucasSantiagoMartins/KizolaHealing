6cd2d1902c840fd88e711bcc167fcd5a0ae132a1
lucascody <lucassantiagocody@gmail.com>
2024-08-13 16:12:44 +0100

    Create Address model in health app

--- health/models.py
+++ health/models.py
@@ -6,3 +6,10 @@ class BasicInformation(models.Model):
     institution_name = models.CharField(max_length=150)
     nif = models.CharField(max_length=14)
     institution_type = models.CharField(max_length=3, choices=INSTITUTION_TYPE)
+
+
+class Address(models.Model):
+    street_address = models.CharField(max_length=120)
+    neighborhood = models.CharField(max_length=40)
+    province = models.CharField(max_length=40)
+    postal_code = models.CharField(max_length=4)
