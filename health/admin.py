7dfb0128e1442a79a9079c716fd2efa198e0f6b2
lucascody <lucassantiagocody@gmail.com>
2024-08-13 16:12:57 +0100

    Register Address model in admin site

--- health/admin.py
+++ health/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import BasicInformation
+from .models import BasicInformation, Address
 
 admin.site.register(BasicInformation)
+admin.site.register(Address)
+
+ 
