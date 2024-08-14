43bd7b1db6c2dae3d6be65ebb2f653d7b02f3378
lucascody <lucassantiagocody@gmail.com>
2024-08-14 20:05:28 +0100

    Rename health app License model comments field to scope

--- health/models.py
+++ health/models.py
@@ -90,7 +90,7 @@ class License(models.Model):
     issuing_authority = models.CharField(max_length=100)
     renewal_required = models.BooleanField(default=True)
     renewal_date = models.DateField(null=True, blank=True)
-    comments = models.TextField(null=True, blank=True)
+    scope = models.TextField(null=True, blank=True)
 
 
     def __str__(self):
