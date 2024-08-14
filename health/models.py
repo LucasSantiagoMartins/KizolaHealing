d582086e7d6034cec22d9ede82dc9c6fdf776491
lucascody <lucassantiagocody@gmail.com>
2024-08-14 20:22:24 +0100

    Add founding_date field in InstitutionalInformation model from health app

--- health/models.py
+++ health/models.py
@@ -14,6 +14,7 @@ class InstitutionalInformation(models.Model):
     institution_name = models.CharField(max_length=150)
     nif = models.CharField(max_length=14)
     institution_type = models.CharField(max_length=3, choices=INSTITUTION_TYPE)
+    founding_date = models.DateField()
 
     def __str__(self):
         return self.institution_name
