f2b55718b71f56983c31c9e315f12e4412cf8d33
lucascody <lucassantiagocody@gmail.com>
2024-08-15 14:53:55 +0100

    Register health app Institution model in admin site

--- health/admin.py
+++ health/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import InstitutionalInformation, Address
+from .models import (
+    InstitutionalInformation, 
+    Address,
+    Institution
+)
 
 admin.site.register(InstitutionalInformation)
+admin.site.register(Institution)
 admin.site.register(Address)
 
  
