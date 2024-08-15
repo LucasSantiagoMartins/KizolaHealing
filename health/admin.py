694d5f7fb8f0e97715414657c9c4435f15dd2ade
lucascody <lucassantiagocody@gmail.com>
2024-08-15 14:55:14 +0100

    Register health app AdministrativeInformation model in admin site

--- health/admin.py
+++ health/admin.py
@@ -4,11 +4,12 @@ from .models import (
     Address,
     Institution,
     Phone,
+    AdministrativeInformation
 )
 
 admin.site.register(InstitutionalInformation)
 admin.site.register(Institution)
 admin.site.register(Address)
 admin.site.register(Phone)
-
+admin.site.register(AdministrativeInformation)
  
