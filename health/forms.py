f2c7b7b166f2b5dbd35ee51ef0476297ea5378fb
lucascody <lucassantiagocody@gmail.com>
2024-08-16 20:20:56 +0100

    Create AddressForm  in health app

--- health/forms.py
+++ health/forms.py
@@ -23,7 +23,18 @@ class InstitutionalInformationForm(forms.ModelForm):
             'institution_name': forms.TextInput(attrs={'class': 'form-control mb-3'}),
             'nif': forms.TextInput(attrs={'class': 'form-control mb-3'}),
             'institution_type': forms.Select(attrs={'class': 'form-control mb-3'}),
-           'founding_date': forms.TextInput(attrs={'class': 'form-control mb-3'})
+            'founding_date': forms.TextInput(attrs={'class': 'form-control mb-3'})
         }
 
 
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = '__all__'
+        widgets = {
+            'street_address': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'neighborhood': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'province': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'postal_code': forms.TextInput(attrs={'class': 'form-control mb-3'})
+        }
+       
