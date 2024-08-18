9ff7229f9510078c7594bd6947a3d79d714706cd
lucascody <lucassantiagocody@gmail.com>
2024-08-18 16:09:07 +0100

    Remove OperationInformation model from health app forms.py imports

--- health/forms.py
+++ health/forms.py
@@ -3,7 +3,6 @@ from .models import (
     AdministrativeInformation,
     InstitutionalInformation, 
     CertificationDocument,
-    OperationInformation,
     ContactInformation,
     PolicyInformation,
     LicenseDocument,
