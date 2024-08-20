a3f46907bd22b37af2b788e2ace870b6910bde44
lucascody <lucassantiagocody@gmail.com>
2024-08-20 14:24:11 +0100

    Add forms object to store all div forms from health app integrate_insitution.html template in integrate_institution.js file

---
+++ static/health/js/integrate_institution.js
@@ -0,0 +1,16 @@
+let forms = {
+    1: '#div-form-institutional-information',
+    2: '#div-form-administrative-information',
+    3: '#div-form-certification',
+    4: '#div-form-certification-document',
+    5: '#div-form-license',
+    6: '#div-form-license-document',
+    7: '#div-form-address',
+    8: '#div-form-phone',
+    9: '#div-form-contact-information',
+    10: '#div-form-services-offered',
+    11: '#div-form-operating-hour',
+    12: '#div-form-operating-shifts',
+    13: '#div-form-duty-shifts',
+    14: '#div-form-policy-information',
+}
