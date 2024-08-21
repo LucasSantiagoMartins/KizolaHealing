e6b39d6518d8bbf8d594a612044cb6e123b95b52
lucascody <lucassantiagocody@gmail.com>
2024-08-21 20:09:05 +0100

    Rename url pattern for integrate_institution_view from health app

--- health/urls.py
+++ health/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('integrate-institution/', views.integrate_institution_view, name='intergrate_institution_url')
+    path('integrate-institution/', views.integrate_institution_view, name='integrate_institution_url')
 ]
