88945b507aefcdb2506b457935cbd5da20232b4b
lucascody <lucassantiagocody@gmail.com>
2024-09-20 20:47:23 +0100

    Add importation block in health app test_forms.py file to import all health app forms

---
+++ health/tests/test_forms.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from health.forms import (
+    AdministrativeInformationForm,
+    InstitutionalInformationForm,
+    CertificationDocumentForm,
+    ContactInformationForm,
+    PolicyInformationForm,
+    LicenseDocumentForm,
+    OperatingShiftForm,
+    CertificationForm,
+    OperatingHourForm,
+    ServiceOfferedForm,
+    DutyShiftForm,
+    LicenseForm,
+    AddressForm,
+    PhoneForm,
+)
